bcd02e8c7dcb5ba9ae5205ce2e8f9dc1ce5bc472
Melinda H <melinda.yw.hardin@gmail.com>
2020-03-29 22:20:24 -0500

    authorization token

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,8 +1,9 @@
 const router = require("express").Router();
+const auth = require("../../middleware/auth");
 
 //@route 	GET api/auth
 //@desc 	Test route, for now.
 //@access Public
-router.get("/", (req, res) => res.send("Auth route"));
+router.get("/", auth, (req, res) => res.send("Auth route"));
 
 module.exports = router;
--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,9 @@ const router = require("express").Router();
 const { check, validationResult } = require("express-validator");
 const gravatar = require("gravatar");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("config");
+
 const User = require("../../models/Users");
 
 //@route 	POST api/users
@@ -57,7 +60,21 @@ router.post(
       await user.save();
 
       //Return the jsonwebtoken
-      res.send("user registered");
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
+
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
     } catch (err) {
       console.error(err.message);
       res.status(500).send("Server error");
