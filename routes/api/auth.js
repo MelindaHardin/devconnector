3a9cbb2fd6446c63dbf2aaa6565b99796733df13
Melinda H <melinda.yw.hardin@gmail.com>
2020-03-29 22:27:23 -0500

    user token sending back user information

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,9 +1,17 @@
 const router = require("express").Router();
 const auth = require("../../middleware/auth");
-
+const User = require("../../models/Users");
 //@route 	GET api/auth
 //@desc 	Test route, for now.
 //@access Public
-router.get("/", auth, (req, res) => res.send("Auth route"));
+router.get("/", auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select("-password");
+    res.json(user);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("server error");
+  }
+});
 
 module.exports = router;
